b0c4e97b03984072a97657321160cf46ee9cd064
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-17 22:00:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,7 +12,7 @@ def consolidate_cart(cart)
   output
 end
 
-
+def apply_coupons(cart, coupons)
 coupons.each do |coupon|
     if cart.keys.include? coupon[:item]
       if cart[coupon[:item]][:count] >= coupon[:num]
