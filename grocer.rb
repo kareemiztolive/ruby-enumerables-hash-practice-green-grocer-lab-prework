6cc7d372414b210fb5f80efef4e0df3a224d3e7a
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-17 22:24:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ def apply_clearance(cart)
 cart 
 end
 
-def checkout(cart, coupons) 
+def checkout(cart,coupons) 
   total = 0 
   new_cart = consolidate_cart(cart) 
   coupon_cart = apply_coupons(new_cart, coupons) 
