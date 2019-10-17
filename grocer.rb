8cc0ed220afbac84d01edcfbcec5631ad6a7cf09
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-17 22:01:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,15 @@ end
 
 
 
-
+def apply_clearance(cart) 
+  cart.each do |item, attribute_hash| 
+    if attribute_hash[:clearance] == true 
+      attribute_hash[:price] = (attribute_hash[:price] *
+      0.8).round(2) 
+    end 
+  end 
+cart 
+end
 
 def checkout(cart, coupons)
   # code here
