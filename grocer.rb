6c5e65e407b2accc62a3957d39f3f3d88cf199bf
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-17 22:20:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,4 +56,16 @@ def checkout(cart,coupons)
 total = (total * 0.9) if total > 100 total 
 end
 
+def checkout(cart, coupons)
+  # code here
+  consol_cart=consolidate_cart(cart)
+  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+
+  total = 0.0 
+  cart_with_discounts_applied.keys.each do |item|
+    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  end
+  total > 100.00 ? (total * 0.90).round : total
+end
 
