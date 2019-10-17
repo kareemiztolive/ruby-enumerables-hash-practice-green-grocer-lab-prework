ca41360cb77b8694a0ccb53c70c4db3e2fcbeb84
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-17 21:42:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,19 @@
 def consolidate_cart(cart)
-  # code here
+ output = {}
+  cart.each do |item|
+    item_name = item.keys[0]
+    if output[item_name]
+      output[item_name][:count] += 1 
+    else
+      output[item_name] = item[item_name]
+      output[item_name][:count] = 1 
+    end
+  end
+  output
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
